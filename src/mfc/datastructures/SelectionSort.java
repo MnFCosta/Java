0271345a6ff6cec151614487f084851592bcffcc
Manoel Costa <mnfcosta1@gmail.com>
2024-06-17 13:47:31 -0300

    Selection Sort

---
+++ src/mfc/datastructures/SelectionSort.java
@@ -0,0 +1,29 @@
+package mfc.datastructures;
+
+public class SelectionSort {
+    public static void main(String[] args){
+        int[] array = {9,2,7,5,4,8,3,6,1};
+
+        selectionSort(array);
+
+        for(int i: array){
+            System.out.print(i);
+        }
+    }
+
+    private static void selectionSort(int[] array){
+
+        for(int i = 0; i < array.length - 1; i++){
+            int min = i;
+            for(int j = i + 1; j < array.length; j++){
+                if(array[min] > array[j]){
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+    }
+
+}
