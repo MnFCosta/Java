0de5092ecf60090baec2385f67dbce5eb9ca6f5c
Manoel Costa <mnfcosta1@gmail.com>
2024-06-23 17:31:58 -0300

    Breadth First Search

--- src/mfc/datastructures/Graph.java
+++ src/mfc/datastructures/Graph.java
@@ -1,6 +1,7 @@
 package mfc.datastructures;
-
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
     ArrayList<Node> nodes;
@@ -64,6 +65,27 @@ public class Graph {
                 }
             }
             return;
+
+    }
+
+    public void breadthFirstSearch(int src){
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[matrix.length];
+
+        queue.offer(src);
+        visited[src] = true;
+
+        while(queue.size() != 0){
+            src = queue.poll();
+            System.out.println(nodes.get(src).data + " visited");
+
+            for(int i = 0; i < matrix[src].length; i++){
+                if(matrix[src][i] == 1 & !visited[i]){
+                    queue.offer(i);
+                    visited[i] = true;
+                }
+            }
+        }
 
     }
 }
